8a0287978d6f1185cae4915af71fb324a73e7a6e
kaiky rodrigues DIAS <rdkaiky@gmail.com>
2026-05-06 12:46:18 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,17 +1,39 @@
 window.DATA = {
   "estoque": [
+    // ===== PRODUTOS QUE VOCÊ TESTA E NÃO EXISTIAM =====
+    { "cod1": "OFC-1805 E", "cod2": "89885", "ean": "0010004620005", "desc": "FILTRO RACOR", "tipo": "FILTRO", "maquina": "EURO 5", "qtd": 1000, "local": "B42", "estoqueMin": 10, "imgUrl": "https://turbodieselrse.com.br/wp-content/uploads/2025/04/ofc1805efiltrodecombustivel-06703-1-1.png" },
+    
+    // ===== MOBI =====
     { "cod1": "PSL55", "cod2": "72488", "ean": "7891234567890", "desc": "FILTRO DE OLEO MOBI", "tipo": "FILTRO", "maquina": "MOBI", "qtd": 2, "local": "A02", "estoqueMin": 5, "imgUrl": "" },
     { "cod1": "FAP9054/1", "cod2": "114229", "ean": "7891234567891", "desc": "FILTRO AR DO MOTOR", "tipo": "FILTRO", "maquina": "MOBI", "qtd": 2, "local": "A02", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_600613-MLB109281176913_032026-F.webp" },
     { "cod1": "WO120", "cod2": "101601", "ean": "7891234567892", "desc": "FILTRO DE OLEO", "tipo": "FILTRO", "maquina": "MOBI", "qtd": 3, "local": "A02", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_961606-MLB77432353379_072024-F.webp" },
+    
+    // ===== HILUX =====
     { "cod1": "PU9023Z", "cod2": "142562", "ean": "7891234567893", "desc": "FILTRO DE COMBUSTIVEL PRIMARIO", "tipo": "FILTRO", "maquina": "HILUX", "qtd": 12, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_623607-MLB75932269544_042024-B.webp" },
     { "cod1": "KX570D", "cod2": "72472", "ean": "7891234567894", "desc": "FILTRO DE COMBUSTIVEL", "tipo": "FILTRO", "maquina": "HILUX", "qtd": 4, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_635356-MLB108273804923_032026-F.webp" },
-    { "cod1": "CU22032", "cod2": "129781", "ean": "0010004607341", "desc": "FILTRO DE AR CABINE", "tipo": "FILTRO", "maquina": "HILUX", "qtd": 4, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_835839-MLB79990715811_102024-F.webp" },
-    { "cod1": "W7283", "cod2": "142605", "ean": "7891234567896", "desc": "FILTRO DE OLEO LUBRIFICANTE", "tipo": "FILTRO", "maquina": "all", "qtd": 7, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_677019-MLB94665112615_102025-F.webp" },
+    { "cod1": "CU22032", "cod2": "129781", "ean": "7891234567895", "desc": "FILTRO DE AR CABINE", "tipo": "FILTRO", "maquina": "HILUX", "qtd": 4, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_835839-MLB79990715811_102024-F.webp" },
     { "cod1": "ARL2206", "cod2": "129781", "ean": "7891234567897", "desc": "FILTRO AR DO MOTOR", "tipo": "FILTRO", "maquina": "HILUX", "qtd": 7, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_948299-MLB81397220587_122024-F.webp" },
+    
+    // ===== GERAL =====
+    { "cod1": "W7283", "cod2": "142605", "ean": "7891234567896", "desc": "FILTRO DE OLEO LUBRIFICANTE", "tipo": "FILTRO", "maquina": "all", "qtd": 7, "local": "A03", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_677019-MLB94665112615_102025-F.webp" },
+    
+    // ===== EURO 5 / EURO 6 =====
     { "cod1": "OFL-0602E", "cod2": "89900", "ean": "7891234567898", "desc": "FILTRO OLEO DO MOTOR", "tipo": "FILTRO", "maquina": "EURO 6", "qtd": 1000, "local": "B48", "estoqueMin": 10, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_942130-MLB83014109309_032025-F.webp" },
     { "cod1": "P550529", "cod2": "141433", "ean": "7891234567899", "desc": "FILTRO COMBUSTIVEL", "tipo": "FILTRO", "maquina": "EURO 5", "qtd": 1000, "local": "B37", "estoqueMin": 10, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_864692-MLB75819363349_042024-F.webp" },
+    { "cod1": "P954869", "cod2": "004975", "ean": "7891234567904", "desc": "FILTRO DE AR VOLVO", "tipo": "FILTRO", "maquina": "EURO 5 / EURO 6", "qtd": 1000, "local": "B53", "estoqueMin": 10, "imgUrl": "https://cdn.autodoc.de/thumb?id=11828493&m=0&n=0&lng=pt&rev=94078017" },
+    { "cod1": "OFC-1814 E", "cod2": "90262", "ean": "7891234567905", "desc": "PRE FILTRO DE COMBUSTIVEL", "tipo": "FILTRO", "maquina": "all", "qtd": 1000, "local": "B44", "estoqueMin": 10, "imgUrl": "https://filtrosturbo.com.br/wp-content/uploads/imagens-produtos/TCE1804.jpg" },
+    { "cod1": "OFC-1815", "cod2": "90279", "ean": "7891234567906", "desc": "FILTRO COMBUSTIVEL RACOR", "tipo": "FILTRO", "maquina": "EURO 6", "qtd": 1000, "local": "B43", "estoqueMin": 10, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_830489-MLB106391611248_022026-F.webp" },
+    { "cod1": "OFC-1904E", "cod2": "89891", "ean": "7891234567907", "desc": "FILTRO DE COMBUSTIVEL", "tipo": "FILTRO", "maquina": "EURO 6", "qtd": 1000, "local": "B47", "estoqueMin": 10, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_910695-MLB106391819456_022026-F.webp" },
+    { "cod1": "H 601/10", "cod2": "84445", "ean": "7891234567908", "desc": "FILTRO DA DIRECAO", "tipo": "FILTRO", "maquina": "EURO 5 / EURO 6", "qtd": 74, "local": "B54", "estoqueMin": 10, "imgUrl": "https://th.bing.com/th/id/R.51d3046765fe43b85c1842ce416242d6" },
+    { "cod1": "20580233", "cod2": "59068", "ean": "7891234567909", "desc": "FILTRO DIRECAO", "tipo": "FILTRO", "maquina": "EURO 5 / EURO 6", "qtd": 46, "local": "B53", "estoqueMin": 10, "imgUrl": "" },
+    
+    // ===== CALDEIRARIA =====
     { "cod1": "PFF 180 Z40", "cod2": "82699", "ean": "7891234567900", "desc": "Disco Flap Polifan Pfc 180 Z40", "tipo": "CALDERARIA", "maquina": "all", "qtd": 30, "local": "D26", "estoqueMin": 5, "imgUrl": "" },
+    
+    // ===== PEÇAS =====
     { "cod1": "140942", "cod2": "140942", "ean": "7891234567901", "desc": "PASTILHA DE FREIO HILUX", "tipo": "PECAS", "maquina": "all", "qtd": 4, "local": "A05", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_986005-MLB78367631128_082024-F.webp" },
+    
+    // ===== PENDENTES =====
     { "cod1": "AKX35323", "cod2": "", "ean": "7891234567902", "desc": "FILTRO DE AR CABINE", "tipo": "FILTRO", "maquina": "MOBI", "qtd": 0, "local": "A02", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_Q_NP_781893-MLB88018067090_072025-B.webp", "status": "PENDENTE" },
     { "cod1": "SA75200", "cod2": "", "ean": "7891234567903", "desc": "LONA DE FREIO L200", "tipo": "PECAS", "maquina": "L200", "qtd": 0, "local": "A09", "estoqueMin": 5, "imgUrl": "https://http2.mlstatic.com/D_NQ_NP_2X_757934-MLB94689876543_102025-F.webp", "status": "PENDENTE" }
   ],
